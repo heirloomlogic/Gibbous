3b8a7a727a1496b43e3589e208b4af4a3606e4d3
Chris Sessions <chris@livefront.com>
2026-06-03 14:24:55 -0400

    Initial commit

---
+++ Gibbous/GibbousApp.swift
@@ -0,0 +1,17 @@
+//
+//  GibbousApp.swift
+//  Gibbous
+//
+//  Created by Chris Sessions on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct GibbousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GibbousTests/GibbousTests.swift
@@ -0,0 +1,19 @@
+//
+//  GibbousTests.swift
+//  GibbousTests
+//
+//  Created by Chris Sessions on 6/3/26.
+//
+
+import Testing
+@testable import Gibbous
+
+struct GibbousTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
